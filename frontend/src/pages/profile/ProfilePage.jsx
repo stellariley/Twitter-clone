bf1069d82b73285e5f26293d46dd766a2ffab682
Stella <stellaarileyy@gmail.com>
2025-01-03 00:01:30 +0600

    2

--- frontend/src/pages/profile/ProfilePage.jsx
+++ frontend/src/pages/profile/ProfilePage.jsx
@@ -25,11 +25,11 @@ const ProfilePage = () => {
 
 	const user = {
 		_id: "1",
-		fullName: "John Doe",
-		username: "johndoe",
-		profileImg: "/avatars/boy2.png",
+		fullName: "Stella Riley",
+		username: "stella",
+		profileImg: "/avatars/girl1.png",
 		coverImg: "/cover.png",
-		bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+		bio: "I'm a Web developer.",
 		link: "https://youtube.com/@asaprogrammer_",
 		following: ["1", "2", "3"],
 		followers: ["1", "2", "3"],
